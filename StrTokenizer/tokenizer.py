877a35d21d9e93baf6cdd007b61aacc8c2351e4b
CyberPokemon <imoncoding@gmail.com>
2024-10-13 20:59:28 +0530

    Constructor created

--- StrTokenizer/tokenizer.py
+++ StrTokenizer/tokenizer.py
@@ -1,1 +1,8 @@
-#strTokenizer
+class StrTokenizer:
+    def __init__(self,inputstring,delimeter=" ",return_delims=False):
+        self.inputstr=inputstring
+        self.delim=delimeter
+        self.returndelims=return_delims
+        self.tokens=[]
+        self.index=0
+    
